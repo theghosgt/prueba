77f21b0f298e1a65657018d4599353a1d799b7ef
hamilton.holgina <hamilton.holguina@cadena.com.co>
2021-09-27 11:02:39 -0500

    new reps

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            persona objPersona = new persona();
+            objPersona.crearPersona("Hamilton", 24, "03-07-1997");
+
+            Console.WriteLine("Nombre: " + objPersona.Nombre + "Edad: " + objPersona.Edad + "Fecha De Nacimiento: " + objPersona.FechaNacimiento);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ensallo
+{
+    public class persona
+    {
+        private string nombre;
+        private int edad;
+        private string fechaNacimiento;
+
+        public string Nombre { get => nombre; set => nombre = value; }//metodo
+        public int Edad { get => edad; set => edad = value; }
+        public string FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }//metodo
+
+        public void crearPersona(string _nombre,int _edad, string _fechaNacimiento)
+        {
+            nombre = _nombre;
+            this.edad = _edad;
+            fechaNacimiento = _fechaNacimiento;
+        }
+    }
+}
