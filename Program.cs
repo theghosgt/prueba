77377d43847cba350b6e293e2d9caec239189b66
iber.ceballoso <iber.ceballoso@cadena.com.co>
2021-09-27 12:12:27 -0500

    asi quedo

--- Program.cs
+++ Program.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ensallo
+namespace ensayo
 {
     class Program
     {
         static void Main(string[] args)
         {
             persona objPersona = new persona();
-            	objPersona.crearPersona("Harri", 24, "03-07-1997");
+            	objPersona.crearPersona("Hoarri", 24, "03-07-1997");
 		        objPersona.crearPersona("Andres", 27, "03-07-1994");
 		        objPersona.crearPersona("Oscar", 21, "26-09-2000");
             Console.WriteLine("Nombre: " + objPersona.Nombre + "Edad: " + objPersona.Edad + "Fecha De Nacimiento: " + objPersona.FechaNacimiento);
