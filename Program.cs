7e9f4244b80495f3263797c11b2b7599816a931e
Acorrea23 <andres.correa@cadena.com.co>
2021-09-27 11:50:31 -0500

    Creación nuevo objeto para crear persona
    Caso de Jira: #PRO-1

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ensallo
         {
             persona objPersona = new persona();
             objPersona.crearPersona("Harri", 27, "03-07-1994");
-
+			objPersona.crearPersona("Andres", 27, "03-07-1994");
             Console.WriteLine("Nombre: " + objPersona.Nombre + "Edad: " + objPersona.Edad + "Fecha De Nacimiento: " + objPersona.FechaNacimiento);
             Console.ReadLine();
 
